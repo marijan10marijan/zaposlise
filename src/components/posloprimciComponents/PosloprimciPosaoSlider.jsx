2268b22383ceb834cc98dcf19f953342631fde52
marijan10marijan <mmarijan39@gmail.com>
2024-09-15 18:49:55 +0200

    changes

--- src/components/posloprimciComponents/PosloprimciPosaoSlider.jsx
+++ src/components/posloprimciComponents/PosloprimciPosaoSlider.jsx
@@ -19,7 +19,7 @@ const data = [
     jobPosition: "developer",
   },
   {
-    image: "/cardBackground1.png",
+    image: "/cardBackground4.png",
     name: "Marica Petrović",
     jobPosition: "vozač",
   },
@@ -65,16 +65,16 @@ const PosloprimciPosaoSlider = () => {
             </button>
           </div>
         ))}
-        {/* {data.map((item, index) => (
+        {data.map((item, index) => (
           <div className={styles.posao__card} key={index}>
-           
+            <div></div>
             <Image src={item.image} alt="pozadinska slika" fill sizes="100%" />
             <p className={styles.posao__card_name}>{item.name}</p>
             <button className={styles.posao__card_job}>
               {item.jobPosition}
             </button>
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   );
